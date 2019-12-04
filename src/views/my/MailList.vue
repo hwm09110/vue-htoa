0ce8bc8fff880b379381d86c8f5b65a03e65f4f3
hwm09110 <771734186@qq.com>
2019-12-04 09:36:03 +0800

    update

--- src/views/my/MailList.vue
+++ src/views/my/MailList.vue
@@ -1,11 +1,135 @@
 <template>
   <div class="page-container">
-    通讯录
+    <h1 class="page-title">通讯录</h1>
+    <div class="search-wrap">
+      <div class="item-box">
+        <Input placeholder="输入人员姓名" class="search-input" />
+      </div>
+      <div class="item-box">
+        <Button type="primary">查询</Button>
+      </div>
+    </div>
+    <div class="div-line"></div>
+    <div class="main-wrap">
+      <div class="tree-wrap">
+        <Tree :data="treeData"></Tree>
+      </div>
+      <div class="content-wrap">
+        <div class="table-wrap">
+          <Table :columns="tableColumns" :data="tableData"></Table>
+        </div>
+        <div class="page-wrap">
+          <Page :total="100" show-total show-elevator />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "MailList"
+  name: "MailList",
+  data() {
+    return {
+      treeData: [
+        {
+          title: '宏途',
+          expand: true,
+          children: [
+              {
+                title: '总经办',
+                expand: true,
+              },
+              {
+                title: '技术',
+                expand: true,
+                children: [
+                  {
+                      title: '开放部'
+                  },
+                  {
+                      title: '研发部'
+                  }
+                ]
+              }
+          ]
+        }
+      ],
+      tableColumns: [
+        {
+          title: '人员名称',
+          key: 'name'
+        },
+        {
+          title: '区域',
+          key: 'area'
+        },
+        {
+          title: '职务',
+          key: 'duty'
+        },
+        {
+          title: '直接上级',
+          key: 'superior'
+        },
+      ],
+      tableData: [
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+        {
+          name: "张三丰",
+          area: "广州",
+          duty: "部门经理",
+          superior: "李帅",
+        },
+      ],
+    }
+  },
+  methods: {
+
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.main-wrap{
+  margin-top: 20px;
+  display: flex;
+  .tree-wrap{
+    width: 200px;
+    max-height: 800px;
+    overflow: auto;
+  }
+  .content-wrap{
+    flex: 1;
+  }
+}
+</style>
