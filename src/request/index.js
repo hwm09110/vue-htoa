6bf2a111072ac6c3ae1c977ab78bd52f25848208
wjq <wjq0312@163.com>
2019-12-12 09:37:23 +0800

    公司设置模块

--- src/request/index.js
+++ src/request/index.js
@@ -2,7 +2,8 @@
 import common from './common'
 import kqxz from './kqxz' //考勤行政模块
 import unitTeam from './unitTeam' //单位组织
+import platform from './platform' //公司设置模块
 
-const API = [ common, kqxz, unitTeam]
+const API = [ common, kqxz, unitTeam, platform]
 
 export default  Object.assign({}, ...API)
